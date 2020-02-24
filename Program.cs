66f12e630ec99091b23fc7139d6a915f79bb0406
koshi <koshi@digitaldolphins.jp>
2020-02-24 17:12:27 +0900

    コード追加

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using Regularexpression.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Regularexpression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ///123
+            ///@\d
+            ///
+            if (args.Count() == 2)
+            {
+                var input = args[0];
+                var patern = args[1];
+                StringExtesion se = new StringExtesion();
+                var output = se.RegexExtension(input, patern);
+                Console.WriteLine(output);
+            }
+            else if (args.Count() == 1)
+            {
+                if (args[0] == "/help")
+                {
+                    Console.WriteLine("exe input patern (replace)");
+                }
+            }
+            else
+            {
+                Console.WriteLine("exe input patern (replace)");
+            }
+        }
+    }
+}
---
+++ Utils/StringExtesion.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Regularexpression.Utils
+{
+    public class StringExtesion
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="patern"></param>
+        /// <returns></returns>
+        public string RegexExtension(string input,string patern) {
+            ///正規化
+            var Match = Regex.Match(input, patern);
+
+            if (Match.Success == true)
+            {
+                return input;
+            }
+            return "";
+        }
+    }
+}
